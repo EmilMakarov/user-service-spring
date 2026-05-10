aa854b216d32b27a02543dfad5405c98ddb2ebab
emilmakarov <emil.makarov@gmail.com>
2026-05-10 22:08:57 +0300

    Initial commit

---
+++ src/test/java/org/example/userservicespring/TestUserServiceSpringApplication.java
@@ -0,0 +1,11 @@
+package org.example.userservicespring;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestUserServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(UserServiceSpringApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
